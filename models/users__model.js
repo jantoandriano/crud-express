fd630c21b80b9bdaf8a05c2b71f1b7ea638f2ef1
Janto Andriano <motulojanto@gmail.com>
2019-09-20 14:19:41 +0700

    update model

--- models/users__model.js
+++ models/users__model.js
@@ -4,6 +4,11 @@ const users = [
         id: 1,
         firstName: 'Janto',
         lastName: "motulo"
+    },
+    {
+        id: 2,
+        firstName: "janti",
+        lastName: "motulo"
     }
 ];
 
